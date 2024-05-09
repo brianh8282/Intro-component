5c65133304e1b51cf3f4da98d68e270b13d9d942
Brian <brianhuang8282@gmail.com>
2024-05-09 00:57:45 -0700

    Finished

---
+++ script.js
@@ -0,0 +1,53 @@
+function check(event) {
+    // Perform validation
+    event.preventDefault();
+    const firstNameInput = document.getElementById('firstName');
+    const lastNameInput = document.getElementById('lastName');
+    const emailInput = document.getElementById('email');
+    const passwordInput = document.getElementById('password');
+
+    const firstname = firstNameInput.value.trim();
+    const lastname = lastNameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
+
+    if (firstname === '') {
+        firstNameInput.classList.add('highlight');
+        firstNameInput.placeholder = '';
+        document.getElementById('firstNameError').style.visibility = 'visible';
+    } else {
+        firstNameInput.classList.remove('highlight');
+        firstNameInput.placeholder = 'First Name';
+        document.getElementById('firstNameError').style.visibility = 'hidden';
+    }
+
+    if (lastname === '') {
+        lastNameInput.classList.add('highlight');
+        lastNameInput.placeholder = '';
+        document.getElementById('lastNameError').style.visibility = 'visible';
+    } else {
+        lastNameInput.classList.remove('highlight');
+        lastNameInput.placeholder = 'Last Name';
+        document.getElementById('lastNameError').style.visibility = 'hidden';
+    }
+
+    if (email === '') {
+        emailInput.classList.add('highlight', '#email::placeholder');
+        emailInput.placeholder = 'email@example/com';
+        document.getElementById('emailError').style.visibility = 'visible';
+    } else {
+        emailInput.classList.remove('highlight');
+        emailInput.placeholder = 'Email Address';
+        document.getElementById('emailError').style.visibility = 'hidden';
+    }
+
+    if (password === '') {
+        passwordInput.classList.add('highlight');
+        passwordInput.placeholder = '';
+        document.getElementById('passwordError').style.visibility = 'visible';
+    } else {
+        passwordInput.classList.remove('highlight');
+        passwordInput.placeholder = 'Password';
+        document.getElementById('passwordError').style.visibility = 'hidden';
+    }
+}
